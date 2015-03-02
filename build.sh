308f709e4263e1bf376d1b45271cf1ce2e8a3a7e
Michael Blakeney <mike@acts18tech.com>
2015-03-01 15:44:43 -0900

    updated to store zip in root

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
-# docker build -t actstech/rcouch .
+docker build -t actstech/rcouch .
 container_id=$(docker run -d actstech/rcouch 2>&1)
-docker cp $container_id:/rcouch/rel/rcouch dist
+docker cp $container_id:/rcouch/rel/rcouch `pwd`
 docker stop $container_id
 docker rm $container_id
 cd dist
